5fc8206a6498f3524e8fcff52055d409277cb5bf
Jesus82 <jesus@easyship.com>
2023-09-09 00:29:42 +0200

    Update 01.init.server.ts

--- plugins/01.init.server.ts
+++ plugins/01.init.server.ts
@@ -1,10 +1,5 @@
 export default defineNuxtPlugin(async nuxtApp => {
   // Init store configs. This is analogous to nuxtServerInit in Nuxt 2
-  const config = nuxtApp.nuxt2Context.$config
   const storeContext = nuxtApp.nuxt2Context.store
-  await Promise.all([
-    storeContext.dispatch('countries/loadCountries', config),
-    storeContext.dispatch('noticeBanner/loadStates', config),
-  ])
-  storeContext.dispatch('countries/setCountriesAvailableForSignup')
+  storeContext.dispatch('people/loadPeople')
 })
